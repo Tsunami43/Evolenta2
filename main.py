0b6a68634c29bf4016ba9f8ba7d769786827a31b
Tsunami43 <n3m0_22@mail.ru>
2024-03-20 01:08:19 +0300

    fourth commit - create operations (+, -, *, /)

--- main.py
+++ main.py
@@ -8,38 +8,52 @@ from consolemenu.items import *
 # Create the menu
 title = "Калькулятор"
 subtitle = "a [выберете действие] b :"
-menu = ConsoleMenu(
-    title=title,
-    subtitle=subtitle
-)
+menu = ConsoleMenu(title=title, subtitle=subtitle)
 
 
-def input_a_b():
-    try:
-        a = float(input("Введите число a:"))
-        b = float(input("Введите число b:"))
-    except Exception as ex:
-        return ex
+# Input value(a) and value(b)
+def custom_decorator(func):
+
+    def input_a_b():
+        try:
+            a = float(input("Введите число a: "))
+            b = float(input("Введите число b: "))
+            # Operation calling
+            return func(a, b)
+
+        except Exception as ex:
+            menu.subtitle = f"Ошибка ввода! Повторите попытку...\n{subtitle}"
+            return ex
+
+    return input_a_b
+
+
+@custom_decorator
+def summary(a, b):
+    menu.subtitle = f"Последняя операция - {a} + {b} = {a+b}\n{subtitle}"
+
+
+@custom_decorator
+def difference(a, b):
+    menu.subtitle = f"Последняя операция - {a} - {b} = {a-b}\n{subtitle}"
+
+
+@custom_decorator
+def multiplication(a, b):
+    menu.subtitle = f"Последняя операция - {a} * {b} = {a*b}\n{subtitle}"
+
+
+@custom_decorator
+def division(a, b):
+    menu.subtitle = f"Последняя операция - {a} / {b} = {a/b}\n{subtitle}"
 
 
 # Create menu items
 items = [
-    FunctionItem(
-        "Сумма (+)",
-        input_a_b
-    ),
-    FunctionItem(
-        "Разница (-)",
-        input_a_b
-    ),
-    FunctionItem(
-        "Произведение (*)",
-        input_a_b
-    ),
-    FunctionItem(
-        "Частное (/)",
-        input_a_b
-    )
+    FunctionItem("Сумма (+)", summary),
+    FunctionItem("Разница (-)", difference),
+    FunctionItem("Произведение (*)", multiplication),
+    FunctionItem("Частное (/)", division),
 ]
 
 # Append items in menu
