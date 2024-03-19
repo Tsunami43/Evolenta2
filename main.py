710c479b62728d4614048634c2e70e43c695340c
Tsunami43 <n3m0_22@mail.ru>
2024-03-20 00:31:50 +0300

    second commit - create menu

--- main.py
+++ main.py
@@ -1,1 +1,44 @@
 # python3.11.6
+
+# Import the necessary packages
+from consolemenu import *
+from consolemenu.items import *
+
+
+# Create the menu
+title = "Калькулятор"
+subtitle = "a [выберете действие] b :"
+menu = ConsoleMenu(
+    title=title,
+    subtitle=subtitle
+)
+
+# Create menu items
+items = [
+    FunctionItem(
+        "Сумма (+)",
+        input,
+        ["Введите число a:"]
+    ),
+    FunctionItem(
+        "Разница (-)",
+        input,
+        ["Введите число a:"]
+    ),
+    FunctionItem(
+        "Произведение (*)",
+        input,
+        ["Введите число a:"]
+    ),
+    FunctionItem(
+        "Частное (/)",
+        input,
+        ["Введите число a:"]
+    )
+]
+
+# Append items in menu
+[menu.append_item(item) for item in items]
+
+# Show menu
+menu.show()
